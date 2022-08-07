e5f6120be972a5bc4f51f26a50c280a604f6e027
shininghyunho <gusgh3315@uos.ac.kr>
2022-08-07 17:55:31 +0900

    add : 로그인한 유저 표시했습니다.

--- src/router/views/LoginView.vue
+++ src/router/views/LoginView.vue
@@ -1,17 +1,21 @@
 <template>
-    <div class="login">
+    <div>
         <Login></Login>
     </div>
 </template>
 
 <script>
 import Login from '@/components/Login.vue';
+import { mapGetters } from 'vuex';
 
 export default {
     name: 'LoginView',
     components: {
         Login
-    }
+    },
+    computed: {
+        ...mapGetters('userStore', ['isLogin'])
+    },
 }
 </script>
 
